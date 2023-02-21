3a62948cf6b63d0e53e77d844f36c83098193db2
mustafa mehel <mehelx5207@gmail.com>
2023-02-21 13:43:54 +0300

    aaa

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const data = require("./data");
+const app = express();
+
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});

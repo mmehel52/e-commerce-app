55074d6c8421c4cec0c35f352b9777cd3e001d3e
mustafa mehel <mehelx5207@gmail.com>
2023-02-21 15:24:01 +0300

    ee

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,35 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>amazona</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
         <main>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/product/:slug" element={<ProductScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/product/:slug" element={<ProductScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">
+            <p> © {new Date().getFullYear()}</p>
+          </div>
+        </footer>
       </div>
     </BrowserRouter>
   );
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useReducer } from "react";
-import { Link } from "react-router-dom";
 import axios from "axios";
+import Product from "../components/Product";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 // import logger from "use-reducer-loger";
 
 const reducer = (state, action) => {
@@ -44,22 +46,13 @@ const HomeScreen = () => {
         ) : error ? (
           <div>{error}</div>
         ) : (
-          products.map((product) => (
-            <div className="product" key={product.slug}>
-              <Link to={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </Link>
-              <div className="product-info">
-                <Link to={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </Link>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))
+          <Row>
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
         )}
       </div>
     </div>

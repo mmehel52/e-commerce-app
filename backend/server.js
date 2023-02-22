78d2815bd7e019c0d4adce5a5e740fb66322b694
mustafa mehel <mehelx5207@gmail.com>
2023-02-22 17:22:43 +0300

    aa

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,19 @@
 const express = require("express");
 const data = require("./data");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
 const app = express();
 
 app.get("/api/products", (req, res) => {

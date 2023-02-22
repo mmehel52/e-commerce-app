c44066d0017dcba2584f27ecaee24d37b3873cdf
mustafa mehel <mehelx5207@gmail.com>
2023-02-22 15:58:03 +0300

    aaa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import Nav from "react-bootstrap/Nav";
 import { useContext } from "react";
 import { Store } from "./Store";
 import CartScreen from "./screens/CartScreen";
+import SigninScreen from "./screens/SigninScreen";
 
 function App() {
   const { state } = useContext(Store);
@@ -41,6 +42,7 @@ function App() {
               <Route path="/" element={<HomeScreen />} />
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
             </Routes>
           </Container>
         </main>

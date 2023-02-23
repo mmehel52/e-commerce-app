4392a1393060e715fa758e364f33e19b1e40217c
mustafa mehel <mehelx5207@gmail.com>
2023-02-23 11:21:40 +0300

    fff

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const data = require("./data");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const seedRouter = require("./routes/seedRoutes.js");
+const productRouter = require("./routes/productRoutes.js");
 
 dotenv.config();
 
@@ -16,23 +18,8 @@ mongoose
 
 const app = express();
 
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
-app.get("/api/products/slug/:slug", (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  product
-    ? res.send(product)
-    : res.status(404).send({ message: "Product Not Found" });
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {

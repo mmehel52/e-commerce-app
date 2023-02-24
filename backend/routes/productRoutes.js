f6a182788ea5ba287858fc3e6e22186bda3166ea
mustafa mehel <mehelx5207@gmail.com>
2023-02-24 17:25:29 +0300

    aaa

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const Product = require("../models/productModel.js");
-
+const expressAsyncHandler = require("express-async-handler");
 const productRouter = express.Router();
 
 productRouter.get("/", async (req, res) => {
   const products = await Product.find();
   res.send(products);
 });
-
+productRouter.get(
+  "/categories",
+  expressAsyncHandler(async (req, res) => {
+    const categories = await Product.find().distinct("category");
+    res.send(categories);
+  })
+);
 productRouter.get("/slug/:slug", async (req, res) => {
   const product = await Product.findOne((x) => x.slug === req.params.slug);
   if (product) {

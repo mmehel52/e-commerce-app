05fa15507f55c5c6685303bbadcffd1ae1a25a87
mustafa mehel <mehelx5207@gmail.com>
2023-02-25 14:24:02 +0300

    ss

--- frontend/src/screens/SearchScreen.js
+++ frontend/src/screens/SearchScreen.js
@@ -1,7 +1,90 @@
-import React from "react";
-
+import axios from "axios";
+import React, { useEffect, useReducer, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { getError } from "../utils";
+import { toast } from "react-toastify";
+import { Helmet } from "react-helmet-async";
 const SearchScreen = () => {
-  return <div>SearchScreen</div>;
+  const navigate = useNavigate();
+  const { search } = useLocation();
+  const sp = new URLSearchParams(search);
+  const category = sp.get("category") || "all";
+  const query = sp.get("query") || "all";
+  const price = sp.get("price") || "all";
+  const rating = sp.get("rating") || "all";
+  const order = sp.get("order") || "newest";
+  const page = sp.get("page") || 1;
+
+  const [{ loading, error, products, pages, countProducts }, dispatch] =
+    useReducer(reducer, {
+      loading: true,
+      error: "",
+    });
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "FETCH_REQUEST":
+        return { ...state, loading: true };
+      case "FETCH_SUCCESS":
+        return {
+          ...state,
+          products: action.payload.products,
+          page: action.payload.page,
+          pages: action.payload.pages,
+          countProducts: action.payload.countProducts,
+          loading: false,
+        };
+      case "FETCH_FAIL":
+        return { ...state, loading: false, error: action.payload };
+      default:
+        return state;
+    }
+  };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        dispatch({ type: "UPDATE_REQUEST" });
+
+        const { data } = await axios.get(
+          `/api/products/search?page=${page}&query=${query}&category=${category}&price=${price}`
+        );
+        dispatch({ type: "FETCH_SUCCESS", payload: data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: getError(error) });
+      }
+    };
+    fetchData();
+  }, [error, page, rating, query, price, order, category]);
+
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const { data } = await axios.get(`/api/products/categories`);
+        setCategories(data);
+      } catch (err) {
+        toast.error(getError(err));
+      }
+    };
+    fetchCategories();
+  }, [dispatch]);
+
+  const getFilterUrl = (filter) => {
+    const filterPage = filter.page || page;
+    const filterCategory = filter.page || category;
+    const filterQuery = filter.page || query;
+    const filterRating = filter.page || rating;
+    const filterPrice = filter.page || price;
+    const sortOrder = filter.page || order;
+    return `/search?category=${filterCategory}&query=${filterQuery}&price=${filterPrice}&rating=${filterRating}`;
+  };
+
+  return (
+    <div>
+      <Helmet>
+        <title>Search Product</title>
+      </Helmet>
+    </div>
+  );
 };
 
 export default SearchScreen;
